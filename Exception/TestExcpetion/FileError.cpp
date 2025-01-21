ffe262ebc0222c591eecd8ae1277f7cc3c145ae8
liad-coj-007 <liadc@campus.technion.ac.il>
2025-01-21 22:14:22 +0200

    Delete Exception/TestExcpetion directory

--- Exception/TestExcpetion/FileError.cpp
+++
@@ -1,4 +0,0 @@
-#include "FileError.h"
-
-const string msg = "Error: Unable to open file: ";
-FileError::FileError(const string &filename):runtime_error(msg + filename){}
--- Exception/TestExcpetion/InvaildName.cpp
+++
@@ -1,4 +0,0 @@
-#include "InvaildName.h"
-const string msg = "u already have a test that named: ";
-
-InvaildName::InvaildName(const string &testname):invalid_argument(msg + testname){}
